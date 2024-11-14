ec4818fe72b415ce44d53dfbda24326bc4d1a211
Rafael <r_albuq@live.concordia.ca>
2024-11-14 18:33:25 -0500

    Revert "906.1-hotfix"
    This reverts commit 2da59dab986360a858c47eed0707b3324e863059.

--- schemas.py
+++ schemas.py
@@ -27,12 +27,6 @@ class TransactionSchema(PlainTransactionSchema):
     members = fields.List(fields.Nested(TransactionMemberSchema()), required=True)
 
 
-class TransactionMemberSchema(Schema):
-    transaction = fields.Nested("TransactionSchema", dump_only=True)  # Updated to ensure this is only for output
-    member_id = fields.Int(required=True)  # Added for direct input reference
-    is_payer = fields.Bool(required=True)
-
-
 class TransactionUpdateSchema(Schema):
     description = fields.Str()
     price = fields.Float()
@@ -42,6 +36,12 @@ class MemberSchema(PlainMemberSchema):
     group_id = fields.Int(load_only=True)
     group = fields.Nested(PlainGroupSchema(), dump_only=True)
     transactions = fields.List(fields.Nested(PlainTransactionSchema()), dump_only=True)
+
+
+class TransactionMemberSchema(Schema):
+    transaction = fields.Nested(TransactionSchema, dump_only=True)  # Updated to ensure this is only for output
+    member_id = fields.Int(required=True)  # Added for direct input reference
+    is_payer = fields.Bool(required=True)
 
 
 class UserSchema(Schema):
