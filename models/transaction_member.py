bba7ee4df6adac313acadff7538279946276907b
Rafael <r_albuq@live.concordia.ca>
2024-11-14 18:33:07 -0500

    Revert "906.3-hotfix"
    This reverts commit 810eab35cb878096e3725a03e2cb4d550409fbb9.

--- models/transaction_member.py
+++ models/transaction_member.py
@@ -4,10 +4,6 @@ from db import db
 class TransactionMember(db.Model):
     __tablename__ = "transaction_member"
 
-    transaction_id = db.Column(db.Integer, db.ForeignKey('transaction.id', ondelete='CASCADE'), primary_key=True)
-    member_id = db.Column(db.Integer, db.ForeignKey('member.id', ondelete='CASCADE'), primary_key=True)
-    is_payer = db.Column(db.Boolean, nullable=False)
-
-    # Define the relationships (optional, for ease of access)
-    transaction = db.relationship('TransactionModel', backref=db.backref('members', lazy='dynamic'))
-    member = db.relationship('MemberModel', backref=db.backref('transactions', lazy='dynamic'))
+    transaction_id = db.Column(db.Integer, db.ForeignKey("transactions.id"), primary_key=True, nullable=False)
+    member_id = db.Column(db.Integer, db.ForeignKey("members.id"), primary_key=True, nullable=False)
+    is_payer = db.Column(db.Boolean, unique=False, default=True, nullable=False)
