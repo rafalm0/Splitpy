eb76bd2d6f5c7ec727a53714cc7c06dd60703c18
Rafael <r_albuq@live.concordia.ca>
2024-11-14 16:07:04 -0500

    adding linkage to payment and is_payer

--- resources/transaction.py
+++ resources/transaction.py
@@ -3,8 +3,8 @@ from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
 
 from db import db
-from models import TransactionModel, GroupModel
-from schemas import TransactionSchema, TransactionUpdateSchema
+from models import TransactionModel, GroupModel, MemberModel, TransactionMember
+from schemas import TransactionSchema, TransactionUpdateSchema, TransactionMemberSchema
 from flask_jwt_extended import get_jwt, jwt_required, get_jwt_identity
 
 blp = Blueprint("Transactions", __name__, description="Operations on transactions")
@@ -20,7 +20,7 @@ class Transaction(MethodView):
         current_user_id = get_jwt_identity()
 
         if group.user_id != current_user_id:
-            abort(403, message="You are not authorized to view or modify this transaction.")
+            abort(403, message="You are not authorized to view this transaction.")
 
         return transaction
 
@@ -50,6 +50,21 @@ class Transaction(MethodView):
 
         transaction.price = transaction_data["price"]
         transaction.description = transaction_data["description"]
+
+        # Handle member linking
+        if "members" in transaction_data:
+            transaction.members.clear()
+            for member_data in transaction_data["members"]:
+                member = MemberModel.query.get_or_404(member_data["member_id"])
+                if member.group_id != group.id:
+                    abort(400, message="Member does not belong to the group.")
+
+                transaction_member_link = TransactionMember(
+                    transaction_id=transaction.id,
+                    member_id=member.id,
+                    is_payer=member_data.get("is_payer", False)
+                )
+                db.session.add(transaction_member_link)
 
         db.session.add(transaction)
         db.session.commit()
@@ -83,11 +98,27 @@ class TransactionList(MethodView):
             abort(403, message="You are not authorized to add a transaction to this group.")
 
         transaction = TransactionModel(**transaction_data)
-
         try:
             db.session.add(transaction)
+            db.session.flush()  # Ensures the transaction has an ID before adding members
+
+            # Handle member linking
+            if "members" in transaction_data:
+                for member_data in transaction_data["members"]:
+                    member = MemberModel.query.get_or_404(member_data["member_id"])
+                    if member.group_id != group.id:
+                        abort(400, message="Member does not belong to the group.")
+
+                    transaction_member_link = TransactionMember(
+                        transaction_id=transaction.id,
+                        member_id=member.id,
+                        is_payer=member_data.get("is_payer", False)
+                    )
+                    db.session.add(transaction_member_link)
+
             db.session.commit()
         except SQLAlchemyError:
+            db.session.rollback()
             abort(500, message="An error occurred while inserting the transaction.")
 
         return transaction
