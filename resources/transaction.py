ce90bc8bf67167ae9ff44336a0613c2ad2b67dbe
Rafael <r_albuq@live.concordia.ca>
2024-11-14 15:42:26 -0500

    905-hotfix
    transaction ownership adjusted

--- resources/transaction.py
+++ resources/transaction.py
@@ -3,7 +3,7 @@ from flask_smorest import Blueprint, abort
 from sqlalchemy.exc import SQLAlchemyError
 
 from db import db
-from models import TransactionModel
+from models import TransactionModel, GroupModel
 from schemas import TransactionSchema, TransactionUpdateSchema
 from flask_jwt_extended import get_jwt, jwt_required, get_jwt_identity
 
@@ -16,9 +16,10 @@ class Transaction(MethodView):
     @blp.response(200, TransactionSchema)
     def get(self, transaction_id):
         transaction = TransactionModel.query.get_or_404(transaction_id)
+        group = GroupModel.query.get_or_404(transaction.group_id)
         current_user_id = get_jwt_identity()
 
-        if transaction.group.user_id != current_user_id:
+        if group.user_id != current_user_id:
             abort(403, message="You are not authorized to view or modify this transaction.")
 
         return transaction
@@ -26,9 +27,10 @@ class Transaction(MethodView):
     @jwt_required()
     def delete(self, transaction_id):
         transaction = TransactionModel.query.get_or_404(transaction_id)
+        group = GroupModel.query.get_or_404(transaction.group_id)
         current_user_id = get_jwt_identity()
 
-        if transaction.group.user_id != current_user_id:
+        if group.user_id != current_user_id:
             abort(403, message="You are not authorized to delete this transaction.")
 
         db.session.delete(transaction)
@@ -39,16 +41,15 @@ class Transaction(MethodView):
     @blp.arguments(TransactionUpdateSchema)
     @blp.response(200, TransactionSchema)
     def put(self, transaction_data, transaction_id):
-        transaction = TransactionModel.query.get(transaction_id)
+        transaction = TransactionModel.query.get_or_404(transaction_id)
+        group = GroupModel.query.get_or_404(transaction.group_id)
         current_user_id = get_jwt_identity()
 
-        if transaction:
-            if transaction.group.user_id != current_user_id:
-                abort(403, message="You are not authorized to update this transaction.")
-            transaction.price = transaction_data["price"]
-            transaction.description = transaction_data["description"]
-        else:
-            abort(404, message="Transaction not found.")
+        if group.user_id != current_user_id:
+            abort(403, message="You are not authorized to update this transaction.")
+
+        transaction.price = transaction_data["price"]
+        transaction.description = transaction_data["description"]
 
         db.session.add(transaction)
         db.session.commit()
@@ -61,15 +62,25 @@ class TransactionList(MethodView):
     @jwt_required()
     @blp.response(200, TransactionSchema(many=True))
     def get(self):
-        user_id = get_jwt_identity()
-        return TransactionModel.query.filter_by(user_id=user_id).all()
+        current_user_id = get_jwt_identity()
+        # Fetch transactions only belonging to groups owned by the logged-in user
+        transactions = (
+            db.session.query(TransactionModel)
+            .join(GroupModel)
+            .filter(GroupModel.user_id == current_user_id)
+            .all()
+        )
+        return transactions
 
     @jwt_required()
     @blp.arguments(TransactionSchema)
     @blp.response(201, TransactionSchema)
     def post(self, transaction_data):
-        user_id = get_jwt_identity()
-        transaction_data["user_id"] = user_id
+        current_user_id = get_jwt_identity()
+        group = GroupModel.query.get_or_404(transaction_data["group_id"])
+
+        if group.user_id != current_user_id:
+            abort(403, message="You are not authorized to add a transaction to this group.")
 
         transaction = TransactionModel(**transaction_data)
 
