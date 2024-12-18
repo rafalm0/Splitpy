1f3a567e9b2a8143df425eef2ac7e2fdcef0b009
Rafael <r_albuq@live.concordia.ca>
2024-12-18 12:35:51 -0500

    main logic re-writing

--- main_logic.py
+++ main_logic.py
@@ -3,32 +3,18 @@ def calculate_balance(transactions, group_id=None):
 
     # First we calculate who owns and who is owned by the respective amount
 
-    transaction_dict = {}
+    debt_dict = {}
     for t in transactions:
-        if group_id == t[3]:
+        if group_id == t[2]:
             continue
         transaction = t[0]
-        t_id = transaction.id
-        price = transaction.price
         person = t[1]
-        debt_dict[person] = 0
-        if t_id not in transaction_dict.keys():
-            transaction_dict[t_id] = {"price": price, "payer": [], "non_payer": []}
-        if t[2]:
-            transaction_dict[t_id]['payer'].append(person)
-        else:
-            transaction_dict[t_id]['non_payer'].append(person)
-
-    for t in transaction_dict.values():
-        qtd_part = len(t['payer']) + len(t['non_payer'])
-        qtd_payers = len(t['payer'])
-        price_person = t['price'] / qtd_part
-        price_payers = t['price'] / qtd_payers
-
-        for p in t['payer']:
-            debt_dict[p] += (price_payers - price_person)
-        for p in t['non_payer']:
-            debt_dict[p] -= price_person
+        paid = t[2]
+        consumed = t[3]
+        if person not in debt_dict.keys():
+            debt_dict[person] = 0
+        debt_dict[person] += paid
+        debt_dict[person] -= consumed
 
     print(debt_dict)
     own_list = []
@@ -57,22 +43,22 @@ def calculate_balance(transactions, group_id=None):
         print(f"Lookign at {own_list[0]} transfering to {owned_list[0]}")
         if own_list[0][1] > owned_list[0][1]:
             settle_transactions.append({'payer': own_list[0][0],
-                                                   'receiver': owned_list[0][0],
-                                                   'amount': owned_list[0][1]})
+                                        'receiver': owned_list[0][0],
+                                        'amount': owned_list[0][1]})
 
             own_list[0][1] -= owned_list[0][1]
             owned_list.pop(0)
         elif own_list[0][1] < owned_list[0][1]:
             settle_transactions.append({'payer': own_list[0][0],
-                                                   'receiver': owned_list[0][0],
-                                                   'amount': own_list[0][1]})
+                                        'receiver': owned_list[0][0],
+                                        'amount': own_list[0][1]})
 
             owned_list[0][1] -= own_list[0][1]
             own_list.pop(0)
         else:
             settle_transactions.append({'payer': own_list[0][0],
-                                                   'receiver': owned_list[0][0],
-                                                   'amount': own_list[0][1]})
+                                        'receiver': owned_list[0][0],
+                                        'amount': own_list[0][1]})
             own_list.pop(0)
             owned_list.pop(0)
 
