299db7fda4cb114ee59ebf904fd73aaf27246a4e
Rafael <r_albuq@live.concordia.ca>
2025-01-27 01:09:50 -0500

    datetime to transaction

--- models/transaction.py
+++ models/transaction.py
@@ -1,4 +1,5 @@
 from db import db
+from datetime import datetime
 
 
 class TransactionModel(db.Model):
@@ -7,6 +8,7 @@ class TransactionModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(80), unique=False, nullable=False)
     group_id = db.Column(db.Integer, db.ForeignKey("groups.id"), unique=False, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now, nullable=True)
     group = db.relationship("GroupModel", back_populates="transactions")
 
     members = db.relationship("MemberModel", back_populates="transactions", secondary="transaction_member")
