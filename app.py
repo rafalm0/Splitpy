90750515aaaf63b956dfa37510e83d062981b85c
Rafael <r_albuq@live.concordia.ca>
2025-02-01 18:07:31 -0500

    increase token validity + balance fix

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ def create_app(db_url=None):
     app.config["API_TITLE"] = "SplitPy"
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=2)
     app.config["OPENAPI_URL_PREFIX"] = "/"
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
